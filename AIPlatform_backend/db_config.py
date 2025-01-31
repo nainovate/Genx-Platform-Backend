60f2e0353e923c0535fb6e0753828f2bdb6c5b3b
Bhavitha-git <pedamallubhavitha@gmail.com>
2025-01-31 16:07:35 +0530

    Design View for Users

--- AIPlatform_backend/db_config.py
+++ AIPlatform_backend/db_config.py
@@ -3,8 +3,8 @@ import os
 import os
 
 
-IP_ADDRESS = "172.10.10.26"  # You can change this value as needed
-MONGO_IP = "172.10.10.26"
+IP_ADDRESS = "172.10.10.95"  # You can change this value as needed
+MONGO_IP = "172.10.10.95"
 
 
 config = { 
